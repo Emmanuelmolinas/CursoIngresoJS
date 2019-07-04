1348dcd263f837f8f3706aa1515cdeb8f4c4f7ef
Emmanuelmolinas <52511491+Emmanuelmolinas@users.noreply.github.com>
2019-07-03 21:23:46 -0300

    ejercicios_1_2_3

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,14 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+	var nombre;
+
+	//	alert("Esto funciona de maravilla"); // alert muestra una ventana.
+	nombre = prompt ("ingrese nombre") ; 
+	alert (nombre);
+	/*Enunciado:
+Bienvenidos. 
+Al presionar el botón, se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla".
+	*/
 }
 
